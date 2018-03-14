e080081b344cfb3368ebc0c5fdaf1a7baa3d1bf4
psycho909 <psychosocial909@gmail.com>
2018-03-14 17:23:25 +0800

    213213

--- dist/assets/publicfacilities.php
+++ dist/assets/publicfacilities.php
@@ -18,132 +18,45 @@ include(Document_root.'/Header.php');
 				</li>
 			</ul>
 			<div id="assets-tab">
-				<div class="assets-create-title mb-3">
-					<a href="/smartbuilding/assets.php" class="assets-create-icon fas fa-chevron-left"></a>
-					<span>編輯資產</span>
-				</div>
-				<div class="row justify-content-center">
-					<div class="col-lg-8 col-md-10 col-sm-8 col-xs-12 col-12">
-						<form class="assets-create-form" action="" method="POST">
-							<span class="edit-title mb-3">資產資料</span>
-							<div class="form-group row">
-								<label for="community" class="text-right col-md-3 col-form-label">所屬社區:</label>
-								<div class="col-md-9 d-flex align-items-center">
-									<span>XXXXXX</span>
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-no" class="text-right col-md-3 col-form-label">
-									<span class="important">*</span>資產編號:
-								</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-no" id="assets-no" placeholder="資產編號...">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-name" class="text-right col-md-3 col-form-label">
-									<span class="important">*</span>資產名稱:
-								</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-name" id="assets-name" placeholder="資產名稱...">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-sort" class="text-right col-md-3 col-form-label">分類:</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-sort" id="assets-sort" placeholder="分類...">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-price" class="text-right col-md-3 col-form-label">價格:</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-price" id="assets-price" placeholder="價格..." value="0">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-amount" class="text-right col-md-3 col-form-label">數量:</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-amount" id="assets-amount" placeholder="數量..." value="0">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-man" class="text-right col-md-3 col-form-label">購置人:</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-man" id="assets-man" placeholder="購置人...">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-buy-date" class="text-right col-md-3 col-form-label">
-									<span class="important">*</span>購置日期:
-								</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control datepicker" name="assets-buy-date" id="assets-buy-date" placeholder="購置日期..." >
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-use-state" class="text-right col-md-3 col-form-label">
-									<span class="important">*</span>使用狀態:
-								</label>
-								<div class="col-md-9">
-									<select class="custom-select" name="assets-use-status">
-										<option selected>選取狀態</option>
-										<option value="One">One</option>
-										<option value="Two">Two</option>
-										<option value="Three">Three</option>
-									</select>
-								</div>
-							</div>
-							<span class="edit-title mb-3">資產移交</span>
-							<div class="form-group row">
-								<label for="assets-watch" class="text-right col-md-3 col-form-label">監交人:</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-watch" id="assets-watch" placeholder="監交人...">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-watch" class="text-right col-md-3 col-form-label">移交人:</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control" name="assets-watch" id="assets-watch" placeholder="移交人...">
-								</div>
-							</div>
-							<div class="form-group row">
-								<label for="assets-use-state" class="text-right col-md-3 col-form-label">
-									<span class="important">*</span>使用狀態:
-								</label>
-								<div class="col-md-9">
-									<select class="custom-select" name="assets-use-status">
-										<option selected>選取狀態</option>
-										<option value="One">One</option>
-										<option value="Two">Two</option>
-										<option value="Three">Three</option>
-									</select>
-								</div>
-							</div>
-							<span class="edit-title mb-3">資產報廢</span>
-							<div class="form-group row">
-								<label for="assets-scrap-date" class="text-right col-md-3 col-form-label">
-									<span class="important">*</span>報廢日期:
-								</label>
-								<div class="col-md-9">
-									<input type="text" class="form-control datepicker" name="assets-scrap-date" id="assets-scrap-date" placeholder="報廢日期..." >
-								</div>
-							</div>
-							<div class="form-group row">
-								<div class="col-md-9 offset-md-3">
-									<button class="btn btn-primary">儲存更新</button>
-									<button class="btn btn-outline-secondary">取消更新</button>
-									<button class="btn btn-outline-danger">刪除該資產</button>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
+				<a href="/smartbuilding/assets/household-create.php" class="btn add-asset-btn mb-3">
+					<span>+</span>新增預約公設
+				</a>
+				<table class="table asset-table">
+					<thead class="thead-light">
+						<tr>
+							<th>預約時段</th>
+							<th>預約公設名稱</th>
+							<th>登記人</th>
+							<th>費用</th>
+							<th>編輯公設預約</th>
+						</tr>
+					</thead>
+					<tbody>
+<?php
+	foreach($data as $var) {
+//		echo $var[asset_no];
+//		echo $var[asset_name];
+//		echo $var[status];
+//		echo $var[price];
+//		echo '<br>';
+?>
+
+						<tr>
+							<td><span><?=$var[asset_no]?></span></td>
+							<td><span><?=$var[asset_name]?></span></td>
+							<td><span><?=$var[status]?></span></td>
+							<td><span><?=$var[price]?></span></td>
+							<td><a href="/smartbuilding/assets/edit-page.php" class="btn btn-outline-secondary">編輯</a></td>
+						</tr>
+<?php
+	}
+?>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
 </div>
-</div>
-
 <?php 
 include(Document_root.'/Footer.php');
 ?>
