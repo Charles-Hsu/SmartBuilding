a8ecdc64e100378b482655f688f74a2ab250b4cc
psycho909 <psychosocial909@gmail.com>
2018-03-27 17:04:18 +0800

    123

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,30 @@
+function getTodayTime(){
+    var str='';
+    var now_date=new Date();
+    var now_Year=now_date.getFullYear();
+    var now_Month=now_date.getMonth()+1;
+    var now_Day=now_date.getDate();
+    var now_Hour=now_date.getHours();
+    var now_Min=now_date.getMinutes();
+    var now_Sec=now_date.getSeconds();
+    if(now_Month < 10){
+        now_Month ='0'+now_Month;
+    }
+    if(now_Day < 10){
+        now_Day ='0'+now_Day;
+    }
+    if(now_Hour < 10){
+        now_Hour ='0'+now_Hour;
+    }
+    if(now_Min < 10){
+        now_Min ='0'+now_Min;
+    }
+    if(now_Sec < 10){
+        now_Sec ='0'+now_Sec;
+    }
+    str=`${now_Year}-${now_Month}-${now_Day} ${now_Hour}:${now_Min}:${now_Sec}`
+    return str;
+}
 $(function(){
     $('.slide-toggle-btn').on('click',function(){
         $('.content-main').toggleClass('toggle');
@@ -10,14 +37,14 @@ $(function(){
             $(this).addClass('fas fa-outdent')
         }
     })
-
+    
     if($('.datepicker').length > 0){
         var dataType=$('.datepicker').attr('data-type')
         if(dataType == 'datetime'){
-            $('.datepicker').cxCalendar({
-                type: 'datetime',
-                format: 'YYYY-MM-DD HH:mm:ss'
-            });
+
+            $('.datepicker').on('focus',function(){
+                $(this).val(getTodayTime())
+            })
         }else{
             $('.datepicker').datepicker({
                 format: 'yyyy-mm-dd',
