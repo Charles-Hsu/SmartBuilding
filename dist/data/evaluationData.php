0e8465543f68692336604541af6c5aadebac0951
psycho909 <psychosocial909@gmail.com>
2018-04-23 08:33:15 +0800

    123

--- dist/data/evaluationData.php
+++ dist/data/evaluationData.php
@@ -1,6 +1,4 @@
 <?php
-
-header('Content-Type: application/json');
 require '../lib/DBAccess.class.php';
 require '../config/config.admin.php';
 
@@ -9,16 +7,29 @@ $db = new DBAccess($conf['db']['dsn'], $conf['db']['user']);
 $evaluation_points=$_POST['evaluation_points'];
 $evaluation_total=$_POST['evaluation_total'];
 
-$manage = json_decode($evaluation_points);
+$evaluation_points=str_replace('\\','',$evaluation_points);
 
-class Msg{
-    public $success='';
-    public $data='';
+$jsonB=json_decode($evaluation_points, true);
+
+foreach($jsonB as $key => $value){
+    echo $key." => ".$value;
 }
 
-$msg=new Msg();
-$msg->success=true;
-$msg->data='成功';
+// print_r($jsonB);
 
 
-echo json_encode($msg);
+// echo $evaluation_points;
+
+
+
+// class Msg{
+//     public $success='';
+//     public $data='';
+// }
+
+// $msg=new Msg();
+// $msg->success=true;
+// $msg->data='成功';
+// echo $manage;
+
+// echo json_encode($msg);
